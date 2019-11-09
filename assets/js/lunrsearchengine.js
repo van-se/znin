c949ab426e4f1919c88be00b002b55f970ff60f7
Tigran <tigran.contact@gmail.com>
2019-11-09 21:34:13 +0400

    Update lunrsearchengine.js

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -21,17 +21,19 @@ var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or p
     "body": "{{ page.date | date: "%Y/%m/%d" }} - {{ page.content | markdownify | replace: '.', '. ' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"{% assign counter = counter | plus: 1 %}
     }{% if forloop.last %}{% else %}, {% endif %}{% endfor %}];
 
-function lunr_search(term) {
+function lunr_search(event) {
+    event.preventDefault();
     $('#lunrsearchresults').show( 400 );
     $( "body" ).addClass( "modal-open" );
     document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Փակել"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Փակել</button></div></div> </div></div>';
-    var _term = term.trim();
+    var searchInput = document.getElementById('lunrsearch').value;
+    var term = searchInput.trim();
     var resultsCount = 0;
-    if(_term) {
+    if(term) {
         document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Որոնման արդիւնքները</h5>" + document.getElementById('modtit').innerHTML;
         //put results on the screen.   
         for(var i = 0; i < documents.length; r++){
-            if(documents[i]['title'].indexOf(_term) >= 0 || documents[i]['body'].indexOf(_term) >= 0){
+            if(documents[i]['title'].indexOf(term) >= 0 || documents[i]['body'].indexOf(term) >= 0){
                 var url = documents[i]['url'];
                 var title = documents[i]['title'];
                 var body = documents[i]['body'].substring(0,160)+'...';
